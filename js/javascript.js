40b01a545a2a124ec456d0ff6f2432667edc2cdf
Lucia <lulypla@gmail.com>
2018-03-06 23:26:17 -0300

    terminado

--- js/javascript.js
+++ js/javascript.js
@@ -1,4 +1,9 @@
 $(function() {
     $('[data-toggle="tooltip"]').tooltip();
     $('[data-toggle="popover"]').popover();
+
+    $('#myList a').on('click', function(e) {
+        e.preventDefault()
+        $(this).tab('show')
+    })
 })
